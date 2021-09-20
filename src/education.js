720c58cb0d8ff396ec5851b040c18b693f205051
Brandon Waller <bwall1331@gmail.com>
2021-09-20 17:18:51 -0400

    small changes

--- src/education.js
+++ src/education.js
@@ -3,7 +3,7 @@ const education=[
         id:1,
         name:"The Ohio State University",
         date:"January 2021-July 2021",
-        course:"Full-Stack Web Development course where we learned how to develop both the fron-end and back-end of a web application",
+        course:"Full-Stack Web Development course where we learned how to develop both the front-end and back-end of a web application",
         cert:"OSU Full-Stack Coding Boot Camp",
         certDate:"July 8, 2021"
     }
