feaf470befd992e14eba9e1d82ebbed4bdc14fb9
Brandon Waller <bwall1331@gmail.com>
2021-11-17 12:48:14 -0500

    ping code to keep awake

--- src/App.js
+++ src/App.js
@@ -48,7 +48,10 @@ function createLink(link){
   )
 }
 
-
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 function App() {
 
